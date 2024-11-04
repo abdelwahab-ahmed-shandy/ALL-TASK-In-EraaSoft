3dc7120a1abe80b5e53e469e42a85381b6cffc59
Abdelwahab Shandy <119771784+abdelwahab-shandy@users.noreply.github.com>
2024-11-04 23:41:50 +0200

    Update Program.cs

--- Task-1/Program.cs
+++ Task-1/Program.cs
@@ -1,3 +1,29 @@
+/*
+    Islam's Carpet Cleaning Service
+    Charges:
+        $25 per small
+        $35 per large
+    Sales tax rate is 6%
+    Estimates are valid for 30 days
+
+    Prompt the user for the number of small and large rooms they would like cleaned
+    and provide an estimate such as:
+
+Estimate for carpet cleaning service
+Number of small carpets: 3 <--
+Number of large carpets: 1 <--
+Price per small room: $25
+Price per large room: $35
+Cost : $110
+Tax: $6.6
+===============================
+Total estimate: $116.6
+This estimate is valid for 30 days
+*/
+
+//Start....
+
+
 using System.Runtime.ConstrainedExecution;
 
 namespace Task_1
