52fc152d2cd2c536c9338066d6e2db537ddcdb4c
Abdelwahab Ahmed Shandy <119771784+abdelwahab-ahmed-shandy@users.noreply.github.com>
2025-03-01 17:08:05 +0200

    update

--- Task-15/Quick Tickets/Areas/Admin/Controllers/ActorsController.cs
+++ Task-15/Quick Tickets/Areas/Admin/Controllers/ActorsController.cs
@@ -1,4 +1,7 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.CodeAnalysis;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Infrastructure;
 
 namespace Quick_Tickets.Areas.Admin.Controllers
 {
@@ -25,63 +28,48 @@ namespace Quick_Tickets.Areas.Admin.Controllers
         [HttpGet]
         public IActionResult Create()
         {
-            ViewBag.Movies = movieRepository.Get().ToList(); // تمرير الأفلام إلى `ViewBag`
+            ViewBag.Movies = movieRepository.Get().ToList();
             return View(new Actor());
         }
 
-        //[HttpPost]
-        //[ValidateAntiForgeryToken]
-        //public IActionResult Create(Actor actor, IFormFile file, List<int> Movies)
-        //{
-        //    if (file != null && file.Length > 0)
-        //    {
-        //        var nameFile = Guid.NewGuid().ToString() + Path.GetExtension(file.FileName);
-        //        var pathFile = Path.Combine(Directory.GetCurrentDirectory(), "wwwroot", "Assats", "image", "Custumer", "ActorProfilePicture", nameFile);
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Create(Actor actor, IFormFile file)
+        {
+            if (ModelState.IsValid)
+            {
+                if (file != null && file.Length > 0)
+                {
+                    var fileName = Guid.NewGuid().ToString() + Path.GetExtension(file.FileName);
+                    var filePath = Path.Combine(Directory.GetCurrentDirectory(), "wwwroot\\Assats\\Custumer\\ActorProfilePicture", fileName);
 
-        //        using (var stream = new FileStream(pathFile, FileMode.Create))
-        //        {
-        //            file.CopyTo(stream);
-        //        }
+                    using (var steam = System.IO.File.Create(filePath))
+                    {
+                        file.CopyTo(steam);
+                    }
 
-        //        actor.ProfilePicture = nameFile;
-        //    }
-        //    else
-        //    {
-        //        ModelState.AddModelError("file", "Please upload an image.");
-        //    }
+                    actor.ProfilePicture = fileName;
 
-        //    if (ModelState.IsValid)
-        //    {
-        //        // حفظ الممثل أولاً
-        //        actorRepository.Create(actor);
-        //        actorRepository.Commit();
+                }
 
-        //        // إضافة العلاقة بين الممثل والأفلام
-        //        if (Movies != null && Movies.Count > 0)
-        //        {
-        //            foreach (var movieId in Movies)
-        //            {
-        //                var actorMovie = new ActorMovie
-        //                {
-        //                    ActorId = actor.Id,
-        //                    MovieId = movieId
-        //                };
-        //                actorRepository(actorMovie);
-        //            }
-        //            actorRepository.Commit();
-        //        }
+                actorRepository.Create(actor);
+                actorRepository.Commit();
 
-        //        return RedirectToAction("Index");
-        //    }
+                return RedirectToAction(nameof(Index));
+            }
 
-        //    ViewBag.Movies = movieRepository.Get().ToList(); // إعادة تحميل قائمة الأفلام عند حدوث خطأ
-        //    return View(actor);
-        //}
+
+            ViewBag.Movies = movieRepository.Get().ToList();
+
+            return View(actor);
+        }
 
         [HttpGet]
         public IActionResult Edit(int Id)
         {
             var ActorEdit = actorRepository.GetOne(a => a.Id == Id);
+
+            ViewBag.Movies = movieRepository.Get().ToList();
 
             if (ActorEdit == null)
             {
@@ -91,20 +79,106 @@ namespace Quick_Tickets.Areas.Admin.Controllers
             return View(ActorEdit);
         }
 
+
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public IActionResult Edit(Actor actor)
+        public IActionResult Edit(Actor actor, IFormFile? file)
         {
-            if (actor != null)
+            if (actor == null)
+                return RedirectToAction("NotFoundPage", "Home", new { area = "Customer" });
+
+            var actorInDatabase = actorRepository.GetOne(e => e.Id == actor.Id, tracked: false);
+
+            if (actorInDatabase == null)
+                return RedirectToAction("NotFoundPage", "Home", new { area = "Customer" });
+
+            // تحديث الصورة في حال رفع صورة جديدة
+            if (file != null && file.Length > 0)
             {
-                actorRepository.Edit(actor);
+                var uploadsFolder = Path.Combine(Directory.GetCurrentDirectory(), "wwwroot\\Assats\\Custumer\\ActorProfilePicture");
+
+                // **ضمان وجود المجلد**
+                if (!Directory.Exists(uploadsFolder))
+                {
+                    Directory.CreateDirectory(uploadsFolder);
+                }
+
+                var fileName = $"{Guid.NewGuid()}{Path.GetExtension(file.FileName)}";
+                var filePath = Path.Combine(uploadsFolder, fileName);
+
+                // حفظ الصورة الجديدة
+                using (var stream = new FileStream(filePath, FileMode.Create))
+                {
+                    file.CopyTo(stream);
+                }
+
+                // حذف الصورة القديمة إن وجدت
+                if (!string.IsNullOrEmpty(actorInDatabase.ProfilePicture))
+                {
+                    var oldImagePath = Path.Combine(uploadsFolder, actorInDatabase.ProfilePicture);
+                    if (System.IO.File.Exists(oldImagePath))
+                    {
+                        System.IO.File.Delete(oldImagePath);
+                    }
+                }
+
+                // تحديث الصورة في الكيان
+                actorInDatabase.ProfilePicture = fileName;
+            }
+
+            // **تحديث البيانات بدلًا من استبدال الكيان بالكامل**
+            actorInDatabase.FirstName = actor.FirstName;
+            actorInDatabase.LastName = actor.LastName;
+            actorInDatabase.Bio = actor.Bio;
+            actorInDatabase.News = actor.News;
+            actorInDatabase.Movies = actor.Movies;
+
+            actorRepository.Edit(actorInDatabase);
+            actorRepository.Commit();
+
+            return RedirectToAction("Index");
+        }
+
+        public IActionResult DeleteImg(int Id)
+        {
+            var actorDelete = actorRepository.GetOne(a => a.Id == Id);
+
+            if (actorDelete != null)
+            {
+                var oldPath = Path.Combine(Directory.GetCurrentDirectory(), "wwwroot\\Assats\\Custumer\\ActorProfilePicture", actorDelete.ProfilePicture);
+
+                if (System.IO.File.Exists(oldPath))
+                {
+                    System.IO.File.Delete(oldPath);
+                }
+
+                actorDelete.ProfilePicture = null;
+                actorRepository.Commit();
+
+                return RedirectToAction("Edit", new { Id });
+            }
+            return RedirectToAction("NotFoundPage", "Home");
+        }
+
+        public IActionResult Delete(int Id)
+        {
+            var actorDelete = actorRepository.GetOne(a => a.Id == Id);
+
+            if (actorDelete.ProfilePicture != null)
+            {
+                var oldPath = Path.Combine(Directory.GetCurrentDirectory(), "wwwroot\\Assats\\Custumer\\ActorProfilePicture", actorDelete.ProfilePicture);
+
+                if (System.IO.File.Exists(oldPath))
+                {
+                    System.IO.File.Delete(oldPath);
+                }
+
+                actorRepository.Delete(actorDelete);
                 actorRepository.Commit();
 
                 return RedirectToAction(nameof(Index));
             }
             return RedirectToAction("NotFoundPage", "Home");
         }
-
-
     }
 }
